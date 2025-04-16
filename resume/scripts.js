9a466b28ad2ade46177c0e73ca9adc4d8dbbd19d
hyeon-oo <ggimgim244@gmail.com>
2025-04-16 22:06:40 +0900

    추가함1

--- resume/scripts.js
+++ resume/scripts.js
@@ -1,33 +1,34 @@
-// 네비게이션 링크에 부드러운 스크롤 기능 추가 및 현재 섹션에 따라 active 클래스 적용
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      const targetID = this.getAttribute('href');
-      const targetSection = document.querySelector(targetID);
-      if (targetSection) {
-        targetSection.scrollIntoView({
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
+// Some code thanks to @chrisgannon
+
+var select = function(s) {
+    return document.querySelector(s);
+  }
   
-  // 스크롤 이벤트를 이용하여 현재 섹션에 해당하는 네비게이션 링크에 active 클래스 추가
-  window.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('.resume-section');
-    const navLinks = document.querySelectorAll('.nav-link');
-    let currentIndex = -1;
+  function randomBetween(min,max)
+  {
+      var number = Math.floor(Math.random()*(max-min+1)+min);
     
-    sections.forEach((section, index) => {
-      const sectionTop = section.offsetTop - 150;
-      if (window.scrollY >= sectionTop) {
-        currentIndex = index;
+      if ( number !== 0 ){
+        return number;
+      }else {
+        return 0.5;
       }
-    });
-    
-    navLinks.forEach(link => link.classList.remove('active'));
-    if (currentIndex !== -1) {
-      navLinks[currentIndex].classList.add('active');
-    }
-  });
+  }
   
+  var tl = new TimelineMax();
+  
+  for(var i = 0; i < 20; i++){
+  
+    var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
+      x: randomBetween(12, 15) * (randomBetween(-1, 1)),
+      y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
+      repeat:-1,
+      repeatDelay:randomBetween(0.2, 0.5),
+      yoyo:true,
+      ease:Elastic.easeOut.config(1, 0.5)
+    })
+  
+    tl.add(t, (i+1)/0.6)
+  }
+  
+  tl.seek(50);
